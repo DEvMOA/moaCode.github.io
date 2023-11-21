71c6e55027910dcb5d6a26d5668053b2e0f5b341
Moussa Ousmane Abdourahmane <113778755+DEvMOA@users.noreply.github.com>
2023-11-21 16:30:12 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit{
       this.zainabFaceSnap = new FaceSnap(
         'Zainab',
         "Chaque jour avec toi est une aventure, une histoire captivante où nos rires créent les chapitres et nos regards écrivent les vers. Ta présence est le trésor qui illumine mes jours, et ta tendresse est le refuge où mon âme trouve la paix.",
-        '/assets/zainab.jpg',
+        "/assets/zainab.jpg",
         new Date(),
         9999
       );
@@ -23,7 +23,7 @@ export class AppComponent implements OnInit{
       this.zainabFaceSnap2 = new FaceSnap(
         'Zainab',
         "Dans le doux éclat de chaque instant partagé, je trouve la lumière de mon bonheur. Ton amour est une mélodie douce qui résonne dans mon cœur, une symphonie envoûtante qui danse dans l'air que nous partageons.",
-        '/assets/zainab2.jpg',
+        "../assets/zainab2.jpg",
         new Date(),
         9999
       );
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit{
       this.zainabFaceSnap3 = new FaceSnap(
         'Zainab',
         "Tes yeux sont des étoiles qui illuminent mes nuits, et ta voix est une douce mélodie qui calme les tempêtes de la vie. À travers les hauts et les bas, c'est avec toi que je veux parcourir ce voyage qu'est l'amour.",
-        '/assets/zainab3.jpg',
+        "./assets/zainab3.jpg",
         new Date(),
         9999
       );
