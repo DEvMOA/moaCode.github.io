020328daadff81ead5df1d4a3776b2dc47a28963
devmoa <moabdourahmane42@gmail.com>
2023-11-22 01:17:05 +0100

    remplacement des images par celles de hawa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,25 +13,25 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
       this.zainabFaceSnap = new FaceSnap(
-        'Zainab',
+        'HAWA',
         "Chaque jour avec toi est une aventure, une histoire captivante où nos rires créent les chapitres et nos regards écrivent les vers. Ta présence est le trésor qui illumine mes jours, et ta tendresse est le refuge où mon âme trouve la paix.",
-        'src/assets/zainab.jpg',
+        './assets/hawa.jpg',
         new Date(),
         9999
       );
 
       this.zainabFaceSnap2 = new FaceSnap(
-        'Zainab',
+        'HAWA',
         "Dans le doux éclat de chaque instant partagé, je trouve la lumière de mon bonheur. Ton amour est une mélodie douce qui résonne dans mon cœur, une symphonie envoûtante qui danse dans l'air que nous partageons.",
-        "./assets/zainab2.jpg",
+        "./assets/hawa2.jpg",
         new Date(),
         9999
       );
       
       this.zainabFaceSnap3 = new FaceSnap(
-        'Zainab',
+        'HAWA',
         "Tes yeux sont des étoiles qui illuminent mes nuits, et ta voix est une douce mélodie qui calme les tempêtes de la vie. À travers les hauts et les bas, c'est avec toi que je veux parcourir ce voyage qu'est l'amour.",
-        "./assets/zainab3.jpg",
+        "./assets/hawa3.jpg",
         new Date(),
         9999
       );
